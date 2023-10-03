3304b998263b02f06017fb2be75381267275066f
Matheus <matheusabreutech@gmail.com>
2023-10-03 18:14:06 -0300

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -10,7 +10,8 @@ const Main = ({
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
     <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
+
+    <S.Description> {description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
