c0f1e2f2c4278d5997bff94402d2b03152eaaee2
Matheus <matheusabreutech@gmail.com>
2023-10-08 23:26:44 -0300

    config: storybook resolve alias

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+
 const config = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
@@ -11,6 +13,9 @@ const config = {
   },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, '../src')
+    }
     return config
   }
 }
